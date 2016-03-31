96386023869e3a1467f97aa1a88f8e6a3c6a4803
Sassoon Kuyumcian <sassoon.kuyumcian@auspost.com.au>
2016-03-31 21:46:47 +1100

    partial commit 2

---
+++ Node.h
@@ -0,0 +1,31 @@
+#ifndef NODE_H_
+#define NODE_H_
+
+#include "Location.h"
+
+/*
+    struct for representing surrounding locations as nodes on a graph
+*/
+struct Node
+{
+    static int newId;
+    int m_id;
+    int m_predecessor;
+    Location m_loc;
+
+    Node(Location loc)
+        : m_id(newId++)
+    {
+        m_loc = loc;
+        m_predecessor = -1;
+    };
+
+    Node(Location loc, int predecessor)
+        : m_id(newId++)
+    {
+        m_loc = loc;
+        m_predecessor = predecessor;
+    };
+};
+
+#endif //NODE_H_
